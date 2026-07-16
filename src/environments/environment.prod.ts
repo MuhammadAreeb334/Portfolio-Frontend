3d8659e1c986f67345d4e9704e7f2b20039265cf
Areeb <muhammadareeb334@gmail.com>
2026-07-16 05:04:23 +0500

    Updated

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  emailjs: {
+    serviceId: 'service_272h6eh',
+    templateId: 'template_7r49p5r',
+    publicKey: 'PyQBoItjNSxkomxOg',
+  },
+};
